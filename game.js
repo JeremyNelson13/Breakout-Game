cbf5e82ba19fce7ce36c4abd71ce7a3cff4a02af
Jeremy Nelson <jeremy.nelson8@gmail.com>
2022-01-28 13:04:30 -0500

    Added paddles

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+//
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 var x = canvas.width/2;
@@ -5,8 +6,13 @@ var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
 
-
+//
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 10, 0, Math.PI*2);
@@ -14,9 +20,18 @@ function drawBall() {
     ctx.fill();
     ctx.closePath();
 }
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+}
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     drawBall();
+    drawPaddle();
     x += dx;
     y += dy;
     if (y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
@@ -25,7 +40,31 @@ function draw() {
     if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    console.log("I'm Working")
+    if (rightPressed) {
+        paddleX += 7;
+    } else if (leftPressed) {
+        paddleX -= 7;
+    }
 }
 
+//
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    } else if(e. key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    } else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+//
 setInterval(draw, 10);
